5d6bf249f57a4f8c01ec4b3b7ec6033ffe7c1b09
root <root@silverstripe-fb263484.c.bitnami-builder.internal>
2015-08-09 18:23:39 +0000

    test commit

---
+++ mysite/_config.php
@@ -0,0 +1,15 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $database;
+$database = 'bitnami_silverstripe';
+ini_set('mysqli.default_socket', '/opt/bitnami/mysql/tmp/mysql.sock');
+
+//Use _ss_environment.php file for configuration
+require_once("conf/ConfigureFromEnv.php");
+
+//Security::setDefaultAdmin('admin','password');
+
+Director::forceSSL(array('/^registration/', '/^Security/','/^admin/'));
